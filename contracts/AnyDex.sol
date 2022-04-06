3ac2d80855e235df1f0f871719b36dbaf8cd446d
keeza <CodeKeeza@protonmail.com>
2022-04-06 17:02:11 +0100

    AnyDex Init

---
+++ contracts/AnyDex.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.0;
+
+import './interfaces/IUniswapFactory.sol';
+import './interfaces/IUniswapPair.sol';
+import './interfaces/IUniswapRouter.sol';
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract AnyDex is Ownable {
+    
+
+
+}
